383d7b3591a8db9988677d2f99c4045dbde7d93c
Yunseong Lee <yunseong.lee@me.com>
2017-04-30 06:35:59 +0900

    Initial setup with toy HTTP server (#2)
    * Initial check-in
    
    * Update README.md

---
+++ HttpServer/HttpServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace HttpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run(); 
+        }
+    }
+    
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {            
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(
+                    "Hello World. The Time is: " + 
+                    DateTime.Now.ToString("hh:mm:ss tt"));
+                                                               
+            });
+        }
+    }
+}
